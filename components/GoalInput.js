b9dce63888988651ec2417ede19af01a43161ecf
chaithrakm1998 <76141182+chaithrakm1998@users.noreply.github.com>
2021-03-10 09:00:48 +0530

    Initial commit

---
+++ components/GoalInput.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
+
+const GoalInput = props => {
+  const [enteredGoal, setEnteredGoal] = useState('');
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    props.onAddGoal(enteredGoal);
+    setEnteredGoal('');
+  };
+
+  return (
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="CANCEL" color="red" onPress={props.onCancel} />
+          </View>
+          <View style={styles.button}>
+            <Button title="ADD" onPress={addGoalHandler} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '101%',
+    borderColor:"black"
+  },
+  input: {
+    width: '100%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    borderColor:'black',
+    marginBottom: 10
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '70%'
+  },
+  button: {
+    width: '70%'
+  }
+});
+
+export default GoalInput;
